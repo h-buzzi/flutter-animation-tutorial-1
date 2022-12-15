cf36ce4efaa0a8daa6cab63142e5942dd584b31b
h-buzzi <h-buzzi>
2022-12-15 20:04:54 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/animation_tutorial_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animation_tutorial_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/animation_page.dart
@@ -0,0 +1,57 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class AnimationPage extends StatefulWidget {
+  const AnimationPage({super.key});
+
+  @override
+  State<AnimationPage> createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+    animation =
+        Tween<double>(begin: 0, end: 2 * math.pi).animate(animController)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              animController.reverse();
+            } else if (status == AnimationStatus.dismissed) {
+              animController.forward();
+            }
+          });
+    animController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Transform.rotate(
+      angle: animation.value,
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(30),
+        child: Image.asset('../assets/github.png'),
+      ),
+    ));
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
+  }
+}
